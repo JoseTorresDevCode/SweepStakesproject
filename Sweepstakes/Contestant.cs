c839746ee79ce00ca41b51aacf1afd23ef3f82d3
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-09-01 10:50:01 -0500

    added get/set to contest values/userinterface

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -8,9 +8,9 @@ namespace Sweepstakes
 {
     public class Contestant
     {
-        public string firstName;
-        public string lastName;
-        public string emailAddress;
-        public string registrationNumber;
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public int registrationNumber { get; set; }
     }
 }
--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -13,7 +13,8 @@ namespace Sweepstakes
 
         public static void ContestantDetails(Contestant contestant)
         {
-
+            int contestantNumber;
+            int totalCount;
             Console.WriteLine("What is your First Name:");
             contestant.firstName = Console.ReadLine();
             Console.WriteLine("What is your Last Name");
@@ -21,6 +22,11 @@ namespace Sweepstakes
             Console.WriteLine("What is your Email Address");
             contestant.emailAddress = Console.ReadLine();
 
+            
+            for (contestantNumber = 0; contestantNumber < totalCount; contestantNumber++)
+            {
+                contestantNumber = contestant.registrationNumber;
+            }
 
 
         }
